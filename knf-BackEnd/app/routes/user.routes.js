d0d0b3413adce8991a6e7077781d21f090bf3a1b
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2020-10-24 14:18:19 +0530

    Update user.routes.js

--- knf-BackEnd/app/routes/user.routes.js
+++ knf-BackEnd/app/routes/user.routes.js
@@ -1,20 +1,18 @@
 module.exports = (app) => {
     const users = require('../controllers/user.controller.js');
 
-    // Create a new Note
+   
     app.post('/users', users.create);
 
-    // Retrieve all Notes
+    
     app.get('/users', users.findAll);
 
-    // Retrieve a single Note with noteId
+   
     app.get('/users/:id', users.findOne);
 
-    // Update a Note with noteId
+  
     app.put('/users/:id', users.update);
 
-    // Delete a Note with noteId
-   // app.delete('/users/:id', notes.delete);
-    // Delete a Note with noteId
+ 
     app.delete('/users/:id', users.delete);
 }
